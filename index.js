cf8e67597190cb3f2700e1225bc1b7f5fe713995
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-07-10 13:46:49 -0500

    js Add Item mostly works except a bit of a display bug

--- index.js
+++ index.js
@@ -6,27 +6,25 @@
 
 //  5)OPTIONAL use event.which and mousedown and keydown to add next to item name if it was entered with submnit buton or ener on keyvboard to show functionality for fun
 
+// 6) Bug to fix: check and delete buttons have no gap between them on generated list item
+
 $(function() {
     $('#js-shopping-list-form').submit(event => {
       // this stops the default form submission behavior
       event.preventDefault();
       const addedItem = $(event.currentTarget).find('#shopping-list-entry').val();
-    //   adds item to ALL existing spans. temporary just to check funtionality
-    $('.shopping-item').text(addedItem);
+
+    $('.shopping-list').append(`<li><span class="shopping-item">${addedItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
     }); 
-   })
+
+
+
+
+
+
+
+
+});
    
 
 
-// block of code that will eventually needed to be pushed to the end of <ul class="shopping-list"> when submitted
-{/* <li>
-   <span class="shopping-item">addedItem</span>
-   <div class="shopping-item-controls">
-     <button class="shopping-item-toggle">
-       <span class="button-label">check</span>
-     </button>
-     <button class="shopping-item-delete">
-       <span class="button-label">delete</span>
-     </button>
-   </div>
- </li> */}
