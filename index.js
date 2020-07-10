94a9fa4063b46da93f3bb14127eae4ca9c1f51b9
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-07-10 14:19:04 -0500

    changed addItem to a js function and added query call to the function at bottom of index.js | Early version of strikethrough function also added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Features
-// 1) button/"add Item" needs to add html into ul/"shopping-list" (plus -enter- on kb). this might use .submit() but then also need to use preventDefault()
+// (1)* button/"add Item" needs to add html into ul/"shopping-list" (plus -enter- on kb). this might use .submit() but then also need to use preventDefault()
 //  2) button/"check"  needs to add .shopping-item__checked class to that li (this?). Pressing again needs to remove it. so toggleClass() is likely
 //  3) button/"delete" needs to .toggle() that li since no display:none css exists right now.
 //  4) hint says i will also need closest() which selects the closest parent to the element sleceted. so if li is in selector then the ul will be grabbed, could be good for strike through?
@@ -8,23 +8,27 @@
 
 // 6) Bug to fix: check and delete buttons have no gap between them on generated list item
 
-$(function() {
+
+// Add Item
+function addItem() {
     $('#js-shopping-list-form').submit(event => {
-      // this stops the default form submission behavior
-      event.preventDefault();
-      const addedItem = $(event.currentTarget).find('#shopping-list-entry').val();
-
-    $('.shopping-list').append(`<li><span class="shopping-item">${addedItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
-    }); 
-
-
+        // this stops the default form submission behavior
+        event.preventDefault();
+        const addedItem = $(event.currentTarget).find('#shopping-list-entry').val();
+        $('.shopping-list').append(`<li><span class="shopping-item">${addedItem}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+    })
+}; 
 
 
+// Strikethrough
+function strikethrough() {
+    $('.shopping-list').on('click', function(event) {
+        $('.shopping-item').toggleClass('shopping-item__checked');
+    });
+};
 
 
 
 
-});
-   
-
-
+$(addItem);
+$(strikethrough);
