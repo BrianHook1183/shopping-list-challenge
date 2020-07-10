9eb3b02a5edcd7b5b4256283167fbf651d91c94e
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-07-10 01:37:12 -0500

    pseudocode for js

---
+++ index.js
@@ -0,0 +1,6 @@
+// Features
+// 1) button/"add Item" needs to add html into ul/"shopping-list" (plus -enter- on kb). this might use .submit() but then also need to use preventDefault()
+//  2) button/"check"  needs to add .shopping-item__checked class to that li (this?). Pressing again needs to remove it. so toggleClass() is likely
+//  3) button/"delete" needs to .toggle() that li since no display:none css exists right now.
+//  4) hint says i will also need closest() which selects the closest parent to the element sleceted. so if li is in selector then the ul will be grabbed, could be good for strike through?
+
